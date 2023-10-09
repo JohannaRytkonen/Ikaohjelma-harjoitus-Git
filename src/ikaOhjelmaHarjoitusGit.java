d06215559aa7f1974627ff87f7e127052be3c814
JohannaRytkonen <johanna.mustasaari@gmail.com>
2023-10-09 06:00:13 +0300

    elakelainen

--- src/ikaOhjelmaHarjoitusGit.java
+++ src/ikaOhjelmaHarjoitusGit.java
@@ -5,11 +5,17 @@ public class ikaOhjelmaHarjoitusGit {
 		// TODO Auto-generated method stub
 
 		
-		int ika = 20;
+		int ika = 70;
 		
 		if (ika >= 0 && ika < 18)
 		{
 			System.out.println("Olet alaikäinen");
+		}
+		
+		else if (ika>= 65)
+			
+		{
+			System.out.println("Olet eläkeläinen");
 		}
 		
 		else
